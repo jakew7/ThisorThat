fcf8faf58542d84e2fa0c0350bade86f8235766e
jakew7 <jakewhite97@hotmail.com>
2021-10-11 14:21:00 -0400

    Add project files.

---
+++ ThisorThat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThisorThat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

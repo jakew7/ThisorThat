76f934a2223a96704546643353e1e053ffa5ebec
jakew7 <jakewhite97@hotmail.com>
2021-12-07 19:03:33 -0500

    Created Test Project and ItemsControllerTest controller

---
+++ ThisorThatTests/ItemsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisorThatTests
+{
+    [TestClass]
+    public class ItemsControllerTests
+    {
+        [TestMethod]
+        public void IndexLoadsIndexView()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+
+
+
+        }
+
+    }
+}

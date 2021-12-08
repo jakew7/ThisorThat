c153889f78fbca90e6725befb004f2896030b400
jakew7 <jakewhite97@hotmail.com>
2021-12-07 19:19:34 -0500

    Created private database with refernce to ThisorThat data. Created TestInitialize and memorydatabase with a unique id. Added EnitityFramkeworkCore.

--- ThisorThatTests/ItemsControllerTests.cs
+++ ThisorThatTests/ItemsControllerTests.cs
@@ -1,20 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// for DbContextBuilder
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// reference for ApplicationDbContext
+using ThisorThat.Data;
 
 namespace ThisorThatTests
 {
     [TestClass]
     public class ItemsControllerTests
     {
+        // Create database to be used in all tests
+        private ApplicationDbContext _context;
+
+        // initializes automatically for global arrangement before every test 
+        [TestInitialize]
+
+        public void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                // unique id
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+
         [TestMethod]
         public void IndexLoadsIndexView()
         {
             // arrange
-
+            var controller = new ItemsControllerTests();
 
             // act
 
